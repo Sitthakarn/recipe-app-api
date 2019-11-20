2b34cc27ff62b04f16bb2d197d07d65296c0628b
Patty <Patty@admins-iMac-2.local>
2019-11-21 00:16:17 +0700

    add unittest

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+def add(x, y):
+    return x + y
+
+def substact(x, y):
+    return y - x
+
+def devide(x, y):
+    if y == 0:
+        return "division by zero"
+    else:
+        return x / y
---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from app.calc import add, substact, devide
+
+class CalcTest(TestCase):
+
+    def test_add_number(self):
+        """Test that add 2 numbers"""
+        self.assertEqual(add(3,8), 11)
+
+    def test_substact_number(self):
+        """Test that substact 2 numbers and return the result"""
+        self.assertEqual(substact(5,11), 6)
+        
+    def test_divide_number(self):
+        """Test devide the numbers"""
+        self.assertEqual(devide(8, 2), 4)
+
+    def test_devide_number_by_zero(self):
+        self.assertEqual(devide(8, 0), "division by zero")
+
