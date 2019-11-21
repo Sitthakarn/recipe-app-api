28c41f7ac08c6853f041698d99f1be8d58301308
dewdsp <sitthakarn.p@gmail.com>
2019-11-21 11:36:42 +0700

    add change commit

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,19 +4,9 @@ def add(x, y):
 def substact(x, y):
     return y - x
 
-<<<<<<< HEAD
-def multiply(x, y):
-    return x * y
-
-=======
->>>>>>> 1b57bdff25f3afcb9366e4454c392ea8e178116a
 def devide(x, y):
     if y == 0:
         return "division by zero"
     else:
-<<<<<<< HEAD
         return x/y
 
-=======
-        return x / y
->>>>>>> 1b57bdff25f3afcb9366e4454c392ea8e178116a
